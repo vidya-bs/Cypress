01fdefb156513d97d6a0b7fa7fba0e44da037870
Abhinavgit12 <109946363+Abhinavgit12@users.noreply.github.com>
2023-01-13 14:48:11 +0530

    Add your commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,22 +2,41 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'xqcsnk',
-  
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    "reportDir": "cypress/reports/mocha",
+    "reportFilename": "cypressReport",
+    charts: true,
+    json: true,
+    "reportPageTitle": "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    overwrite: true 
+  },
+  "reporter": "cypress-mochawesome-reporter",
+  "reporterOptions": {
+      "overwrite": false,
+      "html": false,
+      "json": true
+  },
   e2e: {
     // experimentalSessionAndOrigin: true,
     // testIsolation: "off" ,
     // cacheAcrossSpecs: true,
     setupNodeEvents(on, config) {
-      require("cypress-localstorage-commands/plugin")(on, config);
+      require("cypress-localstorage-commands/plugin")(on, config),
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
       // implement node event listeners here
     },
   },
-    "retries": {
-      // Configure retry attempts for `cypress run`
-      // Default is 0
-      "runMode": 2,
-      // Configure retry attempts for `cypress open`
-      // Default is 0
-      "openMode": 0
-    }
+  "retries": {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    "runMode": 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    "openMode": 0
+  }
 });
---
+++ cypress/cypress.config.js
@@ -0,0 +1,42 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'xqcsnk',
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    "reportDir": "cypress/reports/mocha",
+    "reportFilename": "cypressReport",
+    charts: true,
+    json: true,
+    "reportPageTitle": "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    overwrite: true 
+  },
+  "reporter": "../node_modules/mochawesome/src/mochawesome.js",
+  "reporterOptions": {
+      "overwrite": false,
+      "html": false,
+      "json": true
+  },
+  e2e: {
+    // experimentalSessionAndOrigin: true,
+    // testIsolation: "off" ,
+    // cacheAcrossSpecs: true,
+    setupNodeEvents(on, config) {
+      require("cypress-localstorage-commands/plugin")(on, config),
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+      // implement node event listeners here
+    },
+  },
+  "retries": {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    "runMode": 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    "openMode": 0
+  }
+});

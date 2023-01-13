2ae96427e04a9116ab0d401f213334db128f80df
Abhinav <109946363+Abhinavgit12@users.noreply.github.com>
2023-01-13 10:11:27 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,30 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'xqcsnk',
-  
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     // experimentalSessionAndOrigin: true,
     // testIsolation: "off" ,
     // cacheAcrossSpecs: true,
     setupNodeEvents(on, config) {
       require("cypress-localstorage-commands/plugin")(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
+  "retries": {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    "runMode": 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    "openMode": 0
+  }
 });
